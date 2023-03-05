ccde09a36e3d2f31739a401e73f6d586cf796f02
nantywanty <nant.arun.etc@gmail.com>
2023-03-06 00:09:46 +0800

    added react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import packages
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Import components
 import Home from "./components/Home.js";
@@ -16,29 +16,19 @@ export default function App() {
     return (
     <BrowserRouter>
         <div>
-        <Header />
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/search">Search Listings</Link></li>
-                <li><Link to="/mylistings">My Listings</Link></li>
-                <li><Link to="/listing">Listing</Link></li>
-                <li><Link to="/myorders">My Orders</Link></li>
-                <li><Link to="/order">Order</Link></li>
-                <li><Login /></li>
-            </ul>
-        </nav>
+            <Header />
+            
+            <Routes>
+                <Route path="/" element = {<Home />} />
+                <Route path="/search" element = {<Search />} />
+                <Route path="/mylistings" element = {<MyListings />} />
+                <Route path="/listing" element = {<Listing />} />
+                <Route path="/myorders" element = {<MyOrders />} />
+                <Route path="/order" element = {<Order />} />
+                <Route path="/login" element = {<Login />} />
+            </Routes>
 
-        <Routes>
-            <Route path="/" element = {<Home />} />
-            <Route path="/search" element = {<Search />} />
-            <Route path="/mylistings" element = {<MyListings />} />
-            <Route path="/listing" element = {<Listing />} />
-            <Route path="/myorders" element = {<MyOrders />} />
-            <Route path="/order" element = {<Order />} />
-        </Routes>
-
-        <Footer />
+            <Footer />
         </div>
     </BrowserRouter>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,59 @@
+import { Button, Container, Form, Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 export default function Header() {
     return (
         <div>
-            <p>Header function</p>
+            {[false].map((expand) => (
+                <Navbar key={false} bg="light" expand={false} className="mb-3">
+                    <Container fluid>
+                        <Navbar.Brand as={Link} to="/">GroupBuy</Navbar.Brand>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    Username
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <NavDropdown
+                                        title="Account"
+                                        id={`offcanvasNavbarDropdown-expand-${expand}`}
+                                    >
+                                        <NavDropdown.Item as={Link} to="/login">
+                                            Login
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item as={Link} to="/login">
+                                            Logout
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                    <Nav.Link as={Link} to="/mylistings">My Listings</Nav.Link>
+                                    <Nav.Link as={Link} to="/listing">Listing</Nav.Link>
+                                    <Nav.Link as={Link} to="/myorders">My Orders</Nav.Link>
+                                    <Nav.Link as={Link} to="/order">Order</Nav.Link>
+                                </Nav>
+                                <Form className="d-flex">
+                                    <Form.Control
+                                        type="search"
+                                        placeholder="Search"
+                                        className="me-2"
+                                        aria-label="Search"
+                                    />
+                                    <Button variant="outline-success">
+                                        <Nav.Link as={Link} to="/search">Search Listings</Nav.Link>
+                                    </Button>
+                                </Form>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
         </div>
     );
 }
