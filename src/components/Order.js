9af5fd992e6d8d22077636eff7dc725eacb61459
nantywanty <nant.arun.etc@gmail.com>
2023-03-25 04:39:45 +0800

    updated order form

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,6 +1,31 @@
+import { useState } from 'react';
 import { Card, FloatingLabel, Form, Row, Col, Button } from 'react-bootstrap';
 
 export default function Order() {
+    // Form data
+    const [form, setForm] = useState({
+        quantity: 1,
+        address: '',
+        postal_code: '',
+        contact_details: '',
+    });
+
+    // When an input field is changed
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
+
+    // When submit button is clicked
+    const handleSubmit = (event) => {
+        if (event.currentTarget.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            event.preventDefault();
+            console.log(form);
+        }
+    };
+
     return (
         <div className="p-5" style={{
             backgroundImage: `url("https://png.pngtree.com/thumb_back/fw800/background/20200113/pngtree-chinese-new-year-2020-celebration-sale-template-for-background-and-wallpaper-image_327185.jpg")`,
@@ -11,38 +36,88 @@ export default function Order() {
             alignItems: 'center',
             justifyContent: 'center',
         }}>
-            <Card style={{ width: '40rem' }}>
+            <Card style = {{width: '35rem'}}>
                 <Card.Header as="h5">New Order</Card.Header>
-                <Card.Img className="px-2 pt-2"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTNFJtaMn_Y5zPbpVWA0ojPrD_WqKhvGgnfw&usqp=CAU"/>
+                <Card.Img className="px-3 pt-3"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTNFJtaMn_Y5zPbpVWA0ojPrD_WqKhvGgnfw&usqp=CAU"/>
                 <Card.Body>
                     <Card.Title>Item Name</Card.Title>
-                    <Card.Text>Item Description</Card.Text>
-                    <Form>
+                    <Card.Text>Sold by: </Card.Text>
+                    <Card.Text>Unit price: </Card.Text>
+                    <Card.Text>Description: </Card.Text>
+
+                    <Form noValidate validated={true} onSubmit={handleSubmit}>
                         <Row className="mb-3">
-                            <Col>  
-                                <FloatingLabel controlId="formQuantity" label="Quantity">
-                                    <Form.Select aria-label="Floating label select example">
-                                        <option>1</option>
+                            <Col sm={8}>  
+                                <FloatingLabel controlId="floatingQuantity" label="Quantity">
+                                    <Form.Select 
+                                        as={"input"} 
+                                        name="quantity" 
+                                        placeholder="dummy"
+                                        value={form.quantity} 
+                                        onChange={handleChange} 
+                                        required
+                                    >
+                                        {Array(5).fill(1).map((n, i) => (
+                                            <option key={i}>{1+i}</option>
+                                        ))}
+                                        {/* <option>1</option>
                                         <option>2</option>
                                         <option>3</option>
-                                        <option>4</option>
+                                        <option>4</option> */}
                                     </Form.Select>
                                 </FloatingLabel>
                             </Col>
-                            <Col style={{ display: 'flex', alignItems: 'center' }}>
+                            <Col sm={4} style={{ display: 'flex', alignItems: 'center' }}>
                                 Max available: 4
                             </Col>
                         </Row>
-                        <FloatingLabel className="mb-3" controlId="formAddress" label="Delivery address">
-                            <Form.Control type="email" placeholder="name@example.com" />
+                        <Row className="mb-3">
+                            <Col sm={8}>
+                                <FloatingLabel  controlId="floatingAddress" label="Delivery Address">
+                                    <Form.Control 
+                                        type="text" 
+                                        name="address" 
+                                        placeholder="dummy" 
+                                        value={form.address} 
+                                        onChange={handleChange} 
+                                        required
+                                    />
+                                </FloatingLabel>
+                            </Col>
+                            <Col sm={4}>
+                                <FloatingLabel controlId="floatingPostal" label="Postal Code">
+                                    <Form.Control 
+                                        type="tel" 
+                                        pattern="[0-9]{6}"  
+                                        name="postal_code" 
+                                        placeholder="dummy" 
+                                        value={form.postal_code} 
+                                        onChange={handleChange} 
+                                        required
+                                    />
+                                </FloatingLabel>
+                            </Col>
+                        </Row >
+                        <FloatingLabel className="mb-3" controlId="floatingContact" label="Contact Number">
+                            <Form.Control 
+                                type="tel" 
+                                pattern="[0-9]{8}" 
+                                name="contact_details" 
+                                placeholder="dummy" 
+                                value={form.contact_details} 
+                                onChange={handleChange} 
+                                required/>
                         </FloatingLabel>
-                        <FloatingLabel className="mb-3" controlId="floatingPassword" label="Contact Number">
-                            <Form.Control type="password" placeholder="Password" />
-                        </FloatingLabel>
-                        <Button variant="danger" type="submit">
-                            Submit
-                        </Button>
+                        <Row className="mb-1">
+                            <Col style={{ display: 'flex', alignItems: 'center' }}>
+                                <Card.Title>Total: ${5.00*form.quantity}</Card.Title>
+                            </Col>
+                            <Col xs="auto">
+                                <Button variant="danger" type="submit">Submit</Button>
+                            </Col>
+                        </Row>
                     </Form>
+
                 </Card.Body>
             </Card> 
         </div>
