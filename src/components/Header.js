7bde57812ed587829bd084193613f29136e628cf
Dennis <dennis.awj@gmail.com>
2023-03-29 15:23:47 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,57 +1,77 @@
 // Packages
-import { Button, Container, Form, Nav, Navbar, Offcanvas } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { Search } from 'react-bootstrap-icons';
+import { useState } from "react";
+import {
+  Container,
+  Nav,
+  Navbar,
+  Offcanvas,
+} from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
 // Components
 import Login from "./Login.js";
 
 export default function Header(props) {
-    return (
-        <div>
-            <Navbar bg="light" expand={false} fixed="top">
-                <Container fluid>
-                    <Navbar.Brand as={Link} to="/">
-                        <img src="logo.png" width="200" alt="grouper-logo"/>
-                    </Navbar.Brand>
-                    <Form className="d-flex">
-                        <Form.Control
-                            type="search"
-                            placeholder="Search listings"
-                            className="me-2"
-                            aria-label="Search"
-                            htmlSize={50}
-                        />
-                        <Button variant="danger">
-                            <Nav.Link as={Link} to="/search"><Search /></Nav.Link>
-                        </Button>
-                    </Form>
-                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-false`} />
-                    <Navbar.Offcanvas
-                        id={`offcanvasNavbar-expand-false`}
-                        aria-labelledby={`offcanvasNavbarLabel-expand-false`}
-                        scroll={true}
-                        placement="end"
-                    >
-                        <Offcanvas.Header closeButton>
-                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-false`}>
+  const [searchKey, setSearchKey] = useState("");
+  const navigate = useNavigate();
 
-                                <Login user={props.user} setUser={props.setUser}/>
+  // handler when user clicks on search button
+  function handleSubmit(event) {
+    event.preventDefault();
+    const search = { searchKey };
+    navigate("/search", { state: search });
+  }
 
-                            </Offcanvas.Title>
-                        </Offcanvas.Header>
-                        <Offcanvas.Body>
-                            <Nav className="justify-content-end flex-grow-1 pe-3">
-                                <Nav.Link as={Link} to="/mylistings">My Listings</Nav.Link>
-                                <Nav.Link as={Link} to="/listings">All Listings</Nav.Link>
-                                <Nav.Link as={Link} to="/myorders">My Orders</Nav.Link>
-                                <Nav.Link as={Link} to="/order">Order</Nav.Link>
-                            </Nav>
-                            
-                        </Offcanvas.Body>
-                    </Navbar.Offcanvas>
-                </Container>
-            </Navbar>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar bg="light" expand={false} fixed="top">
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/">
+            <img src="logo.png" width="200" alt="grouper-logo" />
+          </Navbar.Brand>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Search listings"
+                value={searchKey}
+                onChange={(event) => setSearchKey(event.target.value)}
+                required
+              />
+              <input type="submit" value="Search" />
+            </form>
+          </div>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-false`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-false`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-false`}
+            scroll={true}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-false`}>
+                <Login user={props.user} setUser={props.setUser} />
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link as={Link} to="/mylistings">
+                  My Listings
+                </Nav.Link>
+                <Nav.Link as={Link} to="/listings">
+                  All Listings
+                </Nav.Link>
+                <Nav.Link as={Link} to="/myorders">
+                  My Orders
+                </Nav.Link>
+                <Nav.Link as={Link} to="/order">
+                  Order
+                </Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
