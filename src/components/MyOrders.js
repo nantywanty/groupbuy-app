188819e554242532304f12375b991ca09a4d620b
nantywanty <nant.arun.etc@gmail.com>
2023-04-03 20:24:12 +0800

    include axios in package.json and removed dummy data from MyOrders

--- src/components/MyOrders.js
+++ src/components/MyOrders.js
@@ -3,51 +3,6 @@ import { useState, useEffect } from 'react';
 
 export default function MyOrders() {
     
-    // // dummy data
-    // const listing_item1 = {
-    //     owner: "Nant",
-    //     image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTNFJtaMn_Y5zPbpVWA0ojPrD_WqKhvGgnfw&usqp=CAU",
-    //     name: "Fuji Apples",
-    //     description: "Fresh Fuji apples from Japan.",
-    //     unit_price: 5.00,
-    //     remaining_quantity: 5,
-    //     end_date: "2023-03-15",
-    //     delivery_date: "2023-05-15",
-    //     status: "Open"
-    // } 
-
-    // const listing_item2 = {
-    //     owner: "Nant",
-    //     image_url: "https://cdn-prod.medicalnewstoday.com/content/images/articles/272/272782/oranges-in-a-box.jpg",
-    //     name: "Oranges",
-    //     description: "Fresh oranges",
-    //     unit_price: 7.00,
-    //     remaining_quantity: 3,
-    //     end_date: "2023-04-15",
-    //     delivery_date: "2023-05-15",
-    //     status: "Closed"
-    // } 
-
-    // const orders = [
-    //     {
-    //         listing: listing_item1,
-    //         status: 'Order placed',
-    //         quantity: 1,
-    //         address: '83 Redhill Lane',
-    //         postal_code: '150083',
-    //         contact_details: '86992755',
-    //     },
-    //     {
-    //         listing: listing_item2,
-    //         status: 'Order placed',
-    //         quantity: 2,
-    //         address: '280 Bukit Batok East',
-    //         postal_code: '650280',
-    //         contact_details: '81254166',
-    //     },
-    // ]
-    // // end of dummy data
-    
     const [orders, setOrders] = useState(null);
     
     useEffect(() => {
