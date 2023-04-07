42f0326b7b8bd7963bbf6c2c92b8257aeea20634
Dennis <dennis.awj@gmail.com>
2023-04-07 15:48:21 +0800

    Pass state variables to Search, Listings and MyListings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import packages
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from 'react';
+import { useState } from "react";
 
 // Import components
 import Home from "./components/Home.js";
@@ -14,29 +14,51 @@ import Order from "./components/Order.js";
 import Login from "./components/Login.js";
 
 export default function App() {
-    const [user, setUser] = useState(null); // logged-in user
-    const [order, setOrder] = useState(null); // specific order to be displayed in order page
-    const [listing, setListing] = useState(null); // specific order to be displayed in listing/order page
+  const [user, setUser] = useState(null); // logged-in user
+  const [order, setOrder] = useState(null); // specific order to be displayed in order page
+  const [listing, setListing] = useState(null); // specific order to be displayed in listing/order page
 
-    return (
+  return (
     <BrowserRouter>
-        <div className="App" style={{
-            backgroundImage: `url("https://c.stocksy.com/a/wCFA00/z9/2441742.jpg")`,
-            backgroundRepeat: 'repeat',
-            backgroundSize: '700px',
-        }}>
-            <Header user={user} setUser = {setUser}/>
-            <Routes>
-                <Route path="/" element = {<Home />} />
-                <Route path="/search" element = {<Search />} />
-                <Route path="/mylistings" element = {<MyListings />} />
-                <Route path="/listings" element = {<Listings />} />
-                <Route path="/myorders" key={Date.now()} element = {<MyOrders user={user} setOrder={setOrder} setListing={setListing}/>} />
-                <Route path="/order" element = {<Order user={user} order={order} listing={listing}/>} />
-                <Route path="/login" element = {<Login />} />
-            </Routes>
-            <Footer />
-        </div>
+      <div
+        className="App"
+        style={{
+          backgroundImage: `url("https://c.stocksy.com/a/wCFA00/z9/2441742.jpg")`,
+          backgroundRepeat: "repeat",
+          backgroundSize: "700px",
+        }}
+      >
+        <Header user={user} setUser={setUser} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/search"
+            element={<Search setOrder={setOrder} setListing={setListing} />}
+          />
+          <Route path="/mylistings" element={<MyListings user={user} />} />
+          <Route
+            path="/listings"
+            element={<Listings setOrder={setOrder} setListing={setListing} />}
+          />
+          <Route
+            path="/myorders"
+            key={Date.now()}
+            element={
+              <MyOrders
+                user={user}
+                setOrder={setOrder}
+                setListing={setListing}
+              />
+            }
+          />
+          <Route
+            path="/order"
+            element={<Order user={user} order={order} listing={listing} />}
+          />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
-    );
+  );
 }
